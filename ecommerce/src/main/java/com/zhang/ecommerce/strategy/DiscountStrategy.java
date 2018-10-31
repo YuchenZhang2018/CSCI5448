8c22d9a62f688432b403b0a7d900184057637ac3
Yuchen <yuchenzhang.study@gmail.com>
2018-10-30 21:27:42 -0600

    update for part 3

--- ecommerce/src/main/java/com/zhang/ecommerce/strategy/DiscountStrategy.java
+++ ecommerce/src/main/java/com/zhang/ecommerce/strategy/DiscountStrategy.java
@@ -9,32 +9,23 @@ import com.zhang.ecommerce.Order;
 @Component
 public abstract class DiscountStrategy {
 	protected StrategyType  strategyType;
-	public List<String> getStrategyParams() {
-		return strategyParams;
-	}
-
-
-	public void setStrategyParams(List<String> strategyParams) {
-		this.strategyParams = strategyParams;
-	}
-
-
 	protected List<String> strategyParams;
 	
 	
+	public DiscountStrategy() {
+	}
 	public DiscountStrategy(StrategyType strategyType, List<String> strategyParams) {
-		super();
 		this.strategyType = strategyType;
 		this.strategyParams = strategyParams;
 	}
-
-
-	public DiscountStrategy() {
-		super();
-	}
-
-
+	
 	public  abstract double computeDiscount(Order order); 
 	public  abstract StrategyType getStrategyType();	
-
+	
+	public List<String> getStrategyParams() {
+		return strategyParams;
+	}
+	public void setStrategyParams(List<String> strategyParams) {
+		this.strategyParams = strategyParams;
+	}
 }
