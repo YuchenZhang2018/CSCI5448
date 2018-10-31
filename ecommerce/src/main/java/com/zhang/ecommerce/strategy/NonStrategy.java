5a88aa7f9d131e908cc69b0d50754ee4936eae11
Yuchen <yuchenzhang.study@gmail.com>
2018-10-30 21:51:34 -0600

    add init method

--- ecommerce/src/main/java/com/zhang/ecommerce/strategy/NonStrategy.java
+++ ecommerce/src/main/java/com/zhang/ecommerce/strategy/NonStrategy.java
@@ -1,9 +1,18 @@
 package com.zhang.ecommerce.strategy;
 
-import com.zhang.ecommerce.Order;
+import javax.annotation.PostConstruct;
 
+import org.springframework.stereotype.Component;
+
+import com.zhang.ecommerce.Order;
+@Component
 public class NonStrategy extends DiscountStrategy {
 
+	@PostConstruct
+	public void init() {
+		StrategyFactory.registerStrategy(this);
+	}
+	
 	@Override
 	public double computeDiscount(Order order) {
 		return order.getTotalCost();
